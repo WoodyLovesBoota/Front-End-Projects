39351d24aa3dfdbfc95ec63aa6279e194d7eadba
WoodyLovesBoota <woodylovesboota@gmail.com>
2023-04-21 00:01:23 +0900

    add padInt function

--- dday/dday.js
+++ dday/dday.js
@@ -1,6 +1,17 @@
 let target = new Date(2024, 1, 1);
 let main = document.getElementById("clock");
-let content = document.createElement("h2");
+let content = document.createElement("p");
+let content2 = document.createElement("p");
+
+const padInt = (n) => {
+  let res = String(n);
+  if(String(n).length===1) {
+    let arr = String(n).split('');
+    arr.unshift('0');
+    res = arr.join('');
+  }
+  return res;
+}
 
 const calcTime = () => {
   let now = new Date();
@@ -9,25 +20,30 @@ const calcTime = () => {
 
   let lest = Math.floor((targetTime - nowTime) / 1000);
   let lestDate = Math.floor(lest / (60 * 60 * 24));
-  let lestHour = Math.floor((lest - lestDate * (60 * 60 * 24)) / (60 * 60));
-  let lestMinute = Math.floor(
+  let lestHour = padInt(Math.floor((lest - lestDate * (60 * 60 * 24)) / (60 * 60)));
+  let lestMinute = padInt(Math.floor(
     (lest - lestDate * (60 * 60 * 24) - lestHour * (60 * 60)) / 60
-  );
-  let lestSecond = Math.floor(
+  ));
+  let lestSecond = padInt(Math.floor(
     lest - lestDate * (60 * 60 * 24) - lestHour * (60 * 60) - lestMinute * 60
-  );
+  ));
+  
   let clockMessage =
     lestDate +
-    "일 " +
+    "일 ";
+
+  let clockMessage2 =
     lestHour +
-    "시간 " +
+    " : " +
     lestMinute +
-    "분 " +
-    lestSecond +
-    "초";
+    " : " +
+    lestSecond;
+    
 
   content.innerHTML = clockMessage;
   main.appendChild(content);
+  content2.innerHTML = clockMessage2;
+  main.appendChild(content2);
 };
 
 setInterval(calcTime, 1000);
