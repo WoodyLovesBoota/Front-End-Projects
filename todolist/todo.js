e0f88f341a628925cd8a7df8e609d9e8f5281d4c
WoodyLovesBoota <woodylovesboota@gmail.com>
2023-04-22 00:23:08 +0900

    add deleteTodo method

--- todolist/todo.js
+++ todolist/todo.js
@@ -1,5 +1,5 @@
-let todos = localStorage.length === 0 ? {} : JSON.parse(localStorage.getItem("todos"));
 let date = new Date()
+let todos = localStorage.length === 0 ? {} : JSON.parse(localStorage.getItem("todos"));
 
 setTodo = () => {
   let todoItem = document.getElementById("todo-input").value;
@@ -7,19 +7,42 @@ setTodo = () => {
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-Object.values(todos).forEach(e =>{
-  let todoList = document.getElementById("todos");
-  let item = document.createElement("li");
-  let content = document.createElement("span");
-  let button = document.createElement("button");
-  let checkBox = document.createElement("input");
-  checkBox.setAttribute("type", "checkbox");
+const getKeyByValue = (object, value) => {
+  return Object.keys(object).find(key => object[key] === value);
+}
 
-  content.innerText = e;
-  button.innerHTML = "X"
+const deleteTodo = (item) => {
+  let target = item.querySelector("span").innerText;
+  let key = getKeyByValue(todos, target);
+  delete todos[key];
+  localStorage.setItem("todos", JSON.stringify(todos));
+  drawTodos();
+}
+
+const drawTodos = () => {
+  let list = document.getElementById("todos");
+  while(list.children.length >0) {
+    list.removeChild(list.lastChild);
+  }
+  Object.values(todos).forEach(e =>{
+    let todoList = document.getElementById("todos");
+    let item = document.createElement("li");
+    let content = document.createElement("span");
+    let button = document.createElement("button");
+    let checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
   
-  todoList.appendChild(item);
-  item.append(checkBox, content, button);
-})
+    content.innerText = e;
+    button.innerHTML = "X"
+    button.setAttribute("onclick", `deleteTodo(this.parentElement);`);
+  
+    todoList.appendChild(item);
+    item.append(checkBox, content, button);
+  });
+};
+
+window.onload = () => {
+  drawTodos();
+}
 
 // TODO : delete todo
