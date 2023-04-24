9a0dacaacbb85ff28b86a0cf3d57fa8884dfaabc
Woody <woodylovesboota@gmail.com>
2023-04-24 23:35:53 +0900

    add cards of projects

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,3 +1,19 @@
 import projects from "./projects.js";
 console.log(projects);
 
+projects["basic"].forEach((e,i) => {
+    console.log(e)
+    let cardArr = document.getElementsByClassName("project-cards")[0];
+    let card = document.createElement("div");
+    card.setAttribute("class", "card");
+    let link = document.createElement("a");
+    link.setAttribute("href", e['code']);
+    let text = document.createElement("p");
+    text.innerText = e["name"];
+    let image = document.createElement('img');
+    image.setAttribute("src", e["image"]);
+    link.append(text, image);
+    card.appendChild(link);
+    cardArr.appendChild(card);
+});
+
