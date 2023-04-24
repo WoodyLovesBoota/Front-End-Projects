23e54b4640260ae86878671cc6dae20f951dc68b
Woody <woodylovesboota@gmail.com>
2023-04-24 23:35:25 +0900

    add todo project to json file

--- portfolio/projects.js
+++ portfolio/projects.js
@@ -2,14 +2,20 @@ const projects = {
     "basic" : [
         {
             "name" : "Calendar",
-            "code" : "../calendar/calendar.html",
-            "image" : "./image/calendar.jpg",
+            "code" : "../../calendar/calendar.html",
+            "image" : "../image/calendar.jpg",
             "detail" : ""
         },
         {
             "name" : "D-day",
-            "code" : "../dday/dday.html",
-            "image" : "./image/dday.jpg",
+            "code" : "../../dday/dday.html",
+            "image" : "../image/dday.jpg",
+            "detail" : ""
+        },
+        {
+            "name" : "Todo-List",
+            "code" : "../../todo/todo.html",
+            "image" : "../image/todo.jpg",
             "detail" : ""
         }
     ]
