59c442d8715c6f778b1a10726cd90618f8a51595
Woody <woodylovesboota@gmail.com>
2023-04-25 23:44:01 +0900

    add description of todo

--- portfolio/projects.js
+++ portfolio/projects.js
@@ -5,7 +5,7 @@ const calendarDetail = [
     "JavaScript 의 Date object 를 이용하여 간단한 calendar 를 만들었다.\
     <br>< table > element 를 이용하여 달력의 frame 을 구현하였다.\
     <br>특정 날짜를 클릭하면 색이 바뀌는 기능과 오늘 날짜를 표시하는 기능을 구현하였다."
-]
+];
 
 const ddayDetail = [
     "2023-04-19", 
@@ -13,7 +13,18 @@ const ddayDetail = [
     "Date object, Math object",
     "JavaScript 의 내장 object 인 Date 와 Math 를 이용하여 새해 까지의 D-day 를 계산하는 App 을 만들었다.\
     <br>Date object 를 이용하여 현재 시간과 target 시간의 차이를 ms 단위로 가져오는 기능를 구현하였다.\
-    <br>Math object 를 이용하여 차이 시간을 일 시 분 초 단위로 나누어 화면에 표시하는 기능을 구현하였다."]
+    <br>Math object 를 이용하여 차이 시간을 일 시 분 초 단위로 나누어 화면에 표시하는 기능을 구현하였다."
+];
+
+const todoDetail = [
+    "2023-04-21 ~ 2023-04-23", 
+    "6hours", 
+    "localStorage, JS object controll",
+    "Local Stroage 와 input form 을 이용하여 Todo List 를 만들었다.\
+    <br>input form 에 할 일을 기록하면 localStorage 에 저장되고, 이를 화면에 출력해주는 기능을 구현하였다.\
+    <br>Todo 의 체크박스를 클릭하면 한 일로 처리되도록 구현하였고, X 버튼을 누르면 localStrorage 와 화면에서 모두 삭제되도록 구현하였다."
+];
+    
 
 const projects = {
     "basic" : [
@@ -33,7 +44,7 @@ const projects = {
             "name" : "Todo-List",
             "code" : "../../todolist/todo.html",
             "image" : "../image/todo.jpg",
-            "detail" : ""
+            "detail" : todoDetail
         }
     ]
 };
--- todolist/todo.js
+++ todolist/todo.js
@@ -1,7 +1,9 @@
+import projects from "../portfolio/projects.js";
+
 let date = new Date()
 let todos = localStorage.length === 0 ? {} : JSON.parse(localStorage.getItem("todos"));
 
-setTodo = () => {
+const setTodo = () => {
   let todoItem = document.getElementById("todo-input").value;
   todos[date.getTime()] = todoItem;
   localStorage.setItem("todos", JSON.stringify(todos));
@@ -43,7 +45,36 @@ const drawTodos = () => {
   });
 };
 
+const drawDescription = () => {
+  let desc = projects.basic[2].detail;
+  let descDiv = document.querySelector("#description");
+  let due = document.createElement('p');
+  let dueLabel = document.createElement('span');
+  let dueContent = document.createElement('span');
+  dueLabel.innerText = "구현 기간 : ";
+  dueContent.innerText = desc[0];
+  due.append(dueLabel, dueContent);
+  let hour = document.createElement('p');
+  let hourLabel = document.createElement('span');
+  let hourContent = document.createElement('span');
+  hourLabel.innerText = "소모 시간 : ";
+  hourContent.innerText = desc[1];
+  hour.append(hourLabel, hourContent);
+  let skill = document.createElement('p');
+  let skillLabel = document.createElement('span');
+  let skillContent = document.createElement('span');
+  skillLabel.innerText = "사용한 기능 : ";
+  skillContent.innerText = desc[2];
+  skill.append(skillLabel, skillContent);
+  let detail = document.createElement('p');
+  let detailContent = document.createElement('span');
+  detailContent.innerHTML = desc[3];
+  detail.append(detailContent);
+  descDiv.append(due, hour, skill, detail);
+};
+
 window.onload = () => {
   drawTodos();
+  drawDescription();
 }
 
