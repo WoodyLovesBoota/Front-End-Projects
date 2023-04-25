205020400d5b0013d8f36659ed112792f3a04d72
Woody <woodylovesboota@gmail.com>
2023-04-25 22:36:19 +0900

    add calendar detail description

--- portfolio/projects.js
+++ portfolio/projects.js
@@ -4,7 +4,7 @@ const projects = {
             "name" : "Calendar",
             "code" : "../../calendar/calendar.html",
             "image" : "../image/calendar.jpg",
-            "detail" : ""
+            "detail" : calendarDetail
         },
         {
             "name" : "D-day",
@@ -20,5 +20,15 @@ const projects = {
         }
     ]
 };
+
+const calendarDetail = [
+    "2023-04-18", 
+    "2023-04-20", 
+    "5hours", 
+    "Date object,table의 주요 method(insertRow deleteRow insertCell))",
+    "JavaScript 의 Date object 를 이용하여 간단한 calendar 를 만들었다.\
+    <table> element 를 이용하여 달력의 frame 을 구현하였다.\
+    특정 날짜를 클릭하면 색이 바뀌는 기능과 오늘 날짜를 표시하는 기능을 구현하였다."
+]
 
 export default (projects);
