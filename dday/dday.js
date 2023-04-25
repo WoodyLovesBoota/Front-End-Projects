9eee960ade5c6577d5b0d53842cf3d84b6e2b363
Woody <woodylovesboota@gmail.com>
2023-04-25 23:27:07 +0900

    add description of dday

--- dday/dday.js
+++ dday/dday.js
@@ -1,3 +1,5 @@
+import projects from "../portfolio/projects.js";
+
 let target = new Date(2024, 1, 1);
 let main = document.getElementById("clock");
 let content = document.createElement("p");
@@ -41,5 +43,31 @@ const calcTime = () => {
   content2.innerHTML = clockMessage2;
   main.appendChild(content2);
 };
+
+let desc = projects.basic[0].detail;
+let descDiv = document.querySelector("#description");
+let due = document.createElement('p');
+let dueLabel = document.createElement('span');
+let dueContent = document.createElement('span');
+dueLabel.innerText = "구현 기간 : ";
+dueContent.innerText = desc[0];
+due.append(dueLabel, dueContent);
+let hour = document.createElement('p');
+let hourLabel = document.createElement('span');
+let hourContent = document.createElement('span');
+hourLabel.innerText = "소모 시간 : ";
+hourContent.innerText = desc[1];
+hour.append(hourLabel, hourContent);
+let skill = document.createElement('p');
+let skillLabel = document.createElement('span');
+let skillContent = document.createElement('span');
+skillLabel.innerText = "사용한 기능 : ";
+skillContent.innerText = desc[2];
+skill.append(skillLabel, skillContent);
+let detail = document.createElement('p');
+let detailContent = document.createElement('span');
+detailContent.innerHTML = desc[3];
+detail.append(detailContent);
+descDiv.append(due, hour, skill, detail);
 
 setInterval(calcTime, 1000);
--- portfolio/projects.js
+++ portfolio/projects.js
@@ -7,6 +7,13 @@ const calendarDetail = [
     <br>특정 날짜를 클릭하면 색이 바뀌는 기능과 오늘 날짜를 표시하는 기능을 구현하였다."
 ]
 
+const ddayDetail = [
+    "2023-04-19", 
+    "2hours", 
+    "Date object, Math object",
+    "JavaScript 의 내장 object 인 Date 와 Math 를 이용하여 새해 까지의 D-day 를 계산하는 App 을 만들었다.\
+    <br>Date object 를 이용하여 현재 시간과 target 시간의 차이를 ms 단위로 가져오는 기능를 구현하였다.\
+    <br>Math object 를 이용하여 차이 시간을 일 시 분 초 단위로 나누어 화면에 표시하는 기능을 구현하였다."]
 
 const projects = {
     "basic" : [
@@ -20,7 +27,7 @@ const projects = {
             "name" : "D-day",
             "code" : "../../dday/dday.html",
             "image" : "../image/dday.jpg",
-            "detail" : ""
+            "detail" : ddayDetail
         },
         {
             "name" : "Todo-List",
