1bdb85f1dd718c8d145f3113d19bf1c31d77fceb
Woody <woodylovesboota@gmail.com>
2023-04-29 00:45:36 +0900

    memo : add drawMemo function

--- memo/memo.js
+++ memo/memo.js
@@ -1,1 +1,33 @@
 import projects from "../portfolio/projects.js";
+
+let memos =
+  localStorage.getItem("memos") === null
+    ? {}
+    : JSON.parse(localStorage.getItem("memos"));
+
+let textArea = document.querySelector("#memo-content");
+textArea.addEventListener("change", (event) => {
+  setMemo(event);
+});
+
+const setMemo = (event) => {
+  let now = new Date().getTime();
+  let memoContent = event.target.value;
+  memos[now] = memoContent;
+  localStorage.setItem("memos", JSON.stringify(memos));
+};
+
+const drawMemos = () => {
+  Object.values(memos).forEach((e) => {
+    let section = document.querySelector("#memo-lists");
+    let memoCard = document.createElement("div");
+    let content = document.createElement("p");
+    content.innerText = e;
+    memoCard.appendChild(content);
+    section.appendChild(memoCard);
+  });
+};
+
+window.onload = () => {
+  drawMemos();
+};
