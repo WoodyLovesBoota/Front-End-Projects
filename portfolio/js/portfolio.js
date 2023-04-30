3ec7a0a6c33bdb75a8cd2b95b32c8e621c8af485
Woody <woodylovesboota@gmail.com>
2023-04-30 23:18:46 +0900

    portfolio : add TODO comments for next step

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -14,3 +14,23 @@ projects["basic"].forEach((e, i) => {
   card.appendChild(link);
   cardArr.appendChild(card);
 });
+
+/* 
+TODO 
+- survey form like google form
+- landing page 
+  https://www.frontendmentor.io/challenges/huddle-landing-page-with-a-single-introductory-section-B_2Wvxgi0
+- restaurant
+  https://www.geeksforgeeks.org/design-a-webpage-for-online-food-delivery-system-using-html-and-css/
+- portfolio (home aboutus projects achivement contact)
+https://www.frontendmentor.io/challenges/minimalist-portfolio-website-LMy-ZRyiE
+- weather app
+- calculator
+- sign up form
+  https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1
+- job listing
+  https://www.frontendmentor.io/challenges/job-listings-with-filtering-ivstIPCt
+
+  
+https://dev.to/frontendmentor/16-front-end-projects-with-designs-to-help-improve-your-coding-skills-5ajl
+*/
