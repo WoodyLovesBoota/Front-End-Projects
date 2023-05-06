39b46f3cb7695ca586b7caccf7e66afd92225f2a
Woody <woodylovesboota@gmail.com>
2023-05-06 23:43:00 +0900

    calculator : add queryselector of arith

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,7 +1,24 @@
+// Get number cell
 let numbers = document.querySelectorAll(".number");
-let atirhs = document.querySelectorAll(".arith");
 
+// Get arith cell
+let plus = document.querySelector("#add");
+let minus = document.querySelector("#substract");
+let multi = document.querySelector("#multiplier");
+let divider = document.querySelector("#divide");
+let rootArith = document.querySelector("#root");
+let powArith = document.querySelector("#pow");
+let reciporal = document.querySelector("#reciporal");
+let plusMinus = document.querySelector("#plus-minus");
+let spot = document.querySelector("#spot");
+let equal = document.querySelector("#equal");
+let backspace = document.querySelector("#backspace");
+let cancelAll = document.querySelector("#cancel-all");
+let cancelNow = document.querySelector("#cancel-now");
+
+// Get result cell
 let resultNumber = document.querySelector("#result-num");
+let pastNumber = document.querySelector("past-num");
 
 let result = "";
 let now = "";
