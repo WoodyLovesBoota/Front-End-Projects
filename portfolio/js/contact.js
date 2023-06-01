2696c308a3f8badbba0762a53c5300475c9d6a8a
Woody <woodylovesboota@gmail.com>
2023-06-01 22:07:24 +0900

    portfolio : add form handle code

--- portfolio/js/contact.js
+++ portfolio/js/contact.js
@@ -1,2 +1,14 @@
 // TODO
 // form 에 연결되는 js 만들기
+let contactForm = document.querySelector("#contact-form");
+let contactSubmit = document.querySelector("#submit-button");
+let contactName = document.querySelector("#name");
+let contactEmail = document.querySelector("#email");
+let contactMessage = document.querySelector("#message");
+
+contactSubmit.addEventListener("click", () => {
+  let cName = contactName.value;
+  let email = contactEmail.value;
+  let message = contactMessage.value;
+  console.log(cName, email, message);
+});
