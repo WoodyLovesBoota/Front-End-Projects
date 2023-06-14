8f7668707b72922ad040fee88ac74ab6bd8bac38
Woody <130626781+WoodyLovesBoota@users.noreply.github.com>
2023-06-14 16:44:19 +0900

    portfolio : add home layout design template

--- portfolio/js/home.js
+++ portfolio/js/home.js
@@ -1,3 +1,8 @@
+/* TODO : layout design
+ https://www.pinterest.co.kr/pin/48484133480157023/
+
+ */
+
 import { introduce } from "../description.js";
 
 let introduceContent = document.querySelector("#introduce");
