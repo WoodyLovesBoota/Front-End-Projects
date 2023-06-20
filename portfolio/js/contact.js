1ef20fe0b387ea424403022799fe9556190ddf02
Woody <130626781+WoodyLovesBoota@users.noreply.github.com>
2023-06-20 20:15:39 +0900

    portfolio : delete unused code

--- portfolio/js/contact.js
+++ portfolio/js/contact.js
@@ -1,11 +1,11 @@
-let contactForm = document.querySelector("#contact-form");
-let contactSubmit = document.querySelector("#submit-button");
-let contactName = document.querySelector("#name");
-let contactEmail = document.querySelector("#email");
-let contactMessage = document.querySelector("#message");
+// let contactForm = document.querySelector("#contact-form");
+// let contactSubmit = document.querySelector("#submit-button");
+// let contactName = document.querySelector("#name");
+// let contactEmail = document.querySelector("#email");
+// let contactMessage = document.querySelector("#message");
 
-contactSubmit.addEventListener("click", () => {
-  let cName = contactName.value;
-  let email = contactEmail.value;
-  let message = contactMessage.value;
-});
+// contactSubmit.addEventListener("click", () => {
+//   let cName = contactName.value;
+//   let email = contactEmail.value;
+//   let message = contactMessage.value;
+// });
