31428b68550b24a4e3b2ba432c6d4815e994e962
Woody <woodylovesboota@gmail.com>
2023-06-23 19:38:57 +0900

    portfolio : modify TODO

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -63,10 +63,8 @@ window.onload = () => {
 
 /* 
 TODO 
-- survey form like google form
 - landing page 
   https://www.frontendmentor.io/challenges/huddle-landing-page-with-a-single-introductory-section-B_2Wvxgi0
-- portfolio (home aboutus projects achivement contact)
 https://www.frontendmentor.io/challenges/minimalist-portfolio-website-LMy-ZRyiE
 - sign up form
   https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1
