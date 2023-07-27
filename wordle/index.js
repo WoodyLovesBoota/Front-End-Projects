24a8731f4cbd8f724c1b582e0c004fe69e14c60e
Woody <130626781+WoodyLovesBoota@users.noreply.github.com>
2023-07-27 22:54:09 +0900

    add auto cursor method

--- wordle/index.js
+++ wordle/index.js
@@ -1,7 +1,22 @@
 let wordRow = document.querySelectorAll(".word-row");
+let wordCell = document.querySelectorAll(".word-row__cell");
+
+//자동 커서 넘기기
+const moveCursor = (element) => {
+  element.addEventListener("keyup", () => {
+    // 수정 필요 : submit 도 type 이 INPUT 임
+    if (element.nextElementSibling.tagName === "INPUT")
+      element.nextElementSibling.focus();
+  });
+};
+
 wordRow.forEach((e) => {
   e.addEventListener("submit", (event) => {
     event.preventDefault();
     console.log("a");
   });
+});
+
+wordCell.forEach((e) => {
+  moveCursor(e);
 });
