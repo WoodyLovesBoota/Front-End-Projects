354247de62273172bb67e82f5e4a6de0b4387c45
Woody <130626781+WoodyLovesBoota@users.noreply.github.com>
2023-07-27 23:00:31 +0900

    modify error

--- wordle/index.js
+++ wordle/index.js
@@ -4,8 +4,7 @@ let wordCells = document.querySelectorAll(".word-row__cell");
 //자동 커서 넘기기
 const moveCursor = (element) => {
   element.addEventListener("keyup", () => {
-    // 수정 필요 : submit 도 type 이 INPUT 임
-    if (element.nextElementSibling.tagName === "INPUT")
+    if (element.nextElementSibling.getAttribute("type") === "text")
       element.nextElementSibling.focus();
   });
 };
